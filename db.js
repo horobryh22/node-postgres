60f40285ec69348468859f458de1b384356f3a00
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-25 12:53:47 +0300

    finished working

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const {Client} = require('pg');
 
 const client = new Client({
-    connectionString: `postgres://vxaleseatbllol:c09c30c2fb85801040eabd3387e6f4e40f702ee2921b4afbd7b0c5d3716bf101@ec2-34-247-172-149.eu-west-1.compute.amazonaws.com:5432/d2sevu0aom60d6
-`,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
