9d580de5ef55b195264752f681be194ee140c5f8
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-25 12:30:52 +0300

    changed database data

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool ({
-    user: 'postgres',
-    password: '549549ab',
-    host: '127.0.0.1',
+    user: 'vxaleseatbllol',
+    password: 'c09c30c2fb85801040eabd3387e6f4e40f702ee2921b4afbd7b0c5d3716bf101',
+    host: 'ec2-34-247-172-149.eu-west-1.compute.amazonaws.com',
     port: 5432,
-    database: 'node_postgres'
+    database: 'd2sevu0aom60d6'
 });
 
 module.exports = pool;
