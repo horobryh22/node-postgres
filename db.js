c408a538f31158d3612f03f5f53ec820c6af7a51
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-25 12:44:09 +0300

    used Clients from pg instead of Pool

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
-const Pool = require('pg').Pool;
-const pool = new Pool ({
-    user: 'vxaleseatbllol',
-    password: 'c09c30c2fb85801040eabd3387e6f4e40f702ee2921b4afbd7b0c5d3716bf101',
-    host: 'ec2-34-247-172-149.eu-west-1.compute.amazonaws.com',
-    port: 5432,
-    database: 'd2sevu0aom60d6'
+const {Client} = require('pg');
+
+const client = new Client({
+    connectionString: `postgres://vxaleseatbllol:c09c30c2fb85801040eabd3387e6f4e40f702ee2921b4afbd7b0c5d3716bf101@ec2-34-247-172-149.eu-west-1.compute.amazonaws.com:5432/d2sevu0aom60d6
+`,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-module.exports = pool;
+client.connect();
+
+module.exports = client;
