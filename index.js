b4a91c51a779d14946440f7dab2d0ded2ce41b5d
Ilya Khorobrykh <horobryh17@gmail.com>
2022-09-25 10:47:48 +0300

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const entityRouter = require('./routes/entity.routes');
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use('/api', entityRouter);
 
